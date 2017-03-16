ab75f8e276a0ad08741f06e58dfc578e520a0de0
jay <jay@contensive.com>
2017-03-16 12:40:08 -0400

    added rough dotnet code that does the equivalent. Should consider cut-paste to proejcts for now.

---
+++ smtpFileMailerDotNet/smtpFileMailerDotNet/Class1.vb
@@ -0,0 +1,22 @@
+﻿
+Option Strict On
+Option Explicit On
+
+Imports System.Net
+Imports System.Net.Mail
+
+
+Namespace Contensive.SmtpFileMailerDotNet
+    Public Class mainClass
+        Public Sub send(toAddress As String, fromAddress As String, subject As String, body As String, smtpServer As String, attachmentFilename As String)
+            Dim message As New MailMessage(fromAddress, toAddress, subject, body)
+            Dim smtpClient As New SmtpClient(smtpServer)
+            Dim attachment As New Attachment(attachmentFilename)
+            message.Attachments.Add(attachment)
+            'smtpClient.Port = 587
+            'smtpClient.Credentials = New System.Net.NetworkCredential("your mail@gmail.com", "your password")
+            'smtpClient.EnableSsl = True
+            smtpClient.Send(message)
+        End Sub
+    End Class
+End Namespace
